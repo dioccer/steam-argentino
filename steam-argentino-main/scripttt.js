97ffa0454756d15ebcf18d7190860677e7eb7203
NaCroAz <127529800+NaCroAz@users.noreply.github.com>
2023-11-14 12:10:03 -0300

    Delete steam-argentino-main/scripttt.js

--- steam-argentino-main/scripttt.js
+++
@@ -1,9 +0,0 @@
-function mostrarVideo() {
-    document.getElementById('imagen').style.display = 'none';
-    document.getElementById('video').style.display = 'block';
-  }
-  
-  function mostrarImagen() {
-    document.getElementById('video').style.display = 'none';
-    document.getElementById('imagen').style.display = 'block';
-  }
